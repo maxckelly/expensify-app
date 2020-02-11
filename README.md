35a2b8285f1ce598c7fadbbda418cfb50033e54e
Max Kelly <max@revelthat.com>
2020-02-11 17:52:47 +1100

    base project

---
+++ README.md
@@ -0,0 +1,2 @@
+## To run the app locally on your machine with live server do the below commands
+`yarn run dev-server`
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components 
+
+
+// CSS
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p> Boiler Plater</p>, document.getElementById('app'));
