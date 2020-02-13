fb1cbec6623e38166c40d2e2ce4a99efecfe0e44
Max Kelly <max@revelthat.com>
2020-02-13 19:05:48 +1100

    can add and remove expense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,45 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid'; //UUID - ID generator 
+
+// ADD_EXPENSE
+const addExpense = (
+  { 
+    description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0
+  } = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description: description,
+    note: note,
+    amount: amount,
+    createdAt: createdAt
+  }
+});
+
+// REMOVE_EXPENSE
+const removeExpense = ({id} = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id: id
+});
 
 // Expenses reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+    // The below return adds the new expense onto the expense array instead of removing the old.
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      // Filter returns a new array and if the item is not equal (false) to the action id then it will be filtered out.
+      return state.filter(({id}) => id !== action.id)
+      
     default:
       return state;
   };
@@ -33,21 +68,29 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+})
 
+// Add expense
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({ description: 'Rent', amount: 200 }));
 
-const demoState = {
-  expenses: [{
-    id: 'sgoengeo',
-    description: 'January Rent',
-    note: 'This was the final payment made.',
-    amount: 54000,
-    createdAt: 0
-  }],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount', // Date or amount.
-    startDate: undefined,
-    endDate: undefined
-  }
-}
+// Remove expense
+store.dispatch(removeExpense({id: expenseOne.expense.id}))
+
+// const demoState = {
+//   expenses: [{
+//     id: 'sgoengeo',
+//     description: 'January Rent',
+//     note: 'This was the final payment made.',
+//     amount: 54000,
+//     createdAt: 0
+//   }],
+//   filters: {
+//     text: 'rent',
+//     sortBy: 'amount', // Date or amount.
+//     startDate: undefined,
+//     endDate: undefined
+//   }
+// }
