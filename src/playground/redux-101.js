29b3ff269f1a7a14e39905f910880c7751a354fe
Max Kelly <max@revelthat.com>
2020-02-13 16:28:31 +1100

    redux and object destructoring

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,21 +1,39 @@
 import { createStore } from 'redux';
 
+
+// Below is an example of default and object destructor.
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  // The below is an example of dynamic information
+  incrementBy: incrementBy
+});
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+  type: "DECREMENT",
+  decrementBy: decrementBy
+});
+
+const setCount = ({count = 101} = {}) => ({
+  type: "SET",
+  count: count
+})
+
+const resetCount = ({count = 0} = {}) => ({
+  type: "RESET",
+  count: count
+})
+
 const store = createStore((state = { count: 0}, action) => {
 
   switch(action.type) {
 
     case 'INCREMENT':
-       // The below is an example of dynamic information
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
-
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'SET':
       return {
@@ -23,7 +41,7 @@ const store = createStore((state = { count: 0}, action) => {
       };
     case 'RESET': 
       return {
-        count: state.count = 0
+        count: action.count
       };
     default:
       return state;
@@ -38,31 +56,36 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 3
-});
+store.dispatch(incrementCount({incrementBy: 5}))
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+// store.dispatch({
+//   type: 'RESET'
+// });
+
+store.dispatch(resetCount());
 
 // The below stops the console.logs to appear when the state is changed. 
 // unsubscribe();
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy: 10
+// });
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(decrementCount({decrementBy: 5}))
+store.dispatch(decrementCount());
+
+// store.dispatch({
+//   type: 'SET',
+//   count: 101
+// });
+
+store.dispatch(setCount({count: 102}))
 
 
