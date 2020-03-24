d16afde16a645e855474113b8cd53db7242fb100
Max Kelly <max@revelthat.com>
2020-03-24 16:03:15 +1100

    changes

--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -3,7 +3,15 @@ import { shallow } from 'enzyme';
 import EditExpense from './../../components/EditExpense';
 import expenses from './../fixtures/expenses';
 
+let editExpense, removeExpense, history, wrapper;
+
 // Should render Edit expense page
+beforeEach(() => {
+  onSubmit = jest.fn();
+  history = {push: jest.fn()};
+  wrapper = shallow(<EditExpense editExpense={}/> )
+})
+
 
 // Should handle edit Expense - Spies
 
