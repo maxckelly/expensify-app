53b27780ac788f21bc1b286b96a451605438dc55
Max Kelly <max@revelthat.com>
2020-03-25 16:33:45 +1100

    changed formating for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // Linking 
 import {Link} from 'react-router-dom';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
-    <div>
-      <Link to={`/edit/${id}`}> <p>{description}</p> </Link>
-      <p> {amount}</p>
-      <p> {createdAt} </p>
-    </div>
+  <div>
+    <Link to={`/edit/${id}`}> <p>{description}</p> </Link>
+    <p> {numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')} </p>
+  </div>
 );
 
 export default ExpenseListItem;
