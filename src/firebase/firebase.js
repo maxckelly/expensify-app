e2629c12e9c6b420b19763dc5ad326dffe10abe1
Max Kelly <max@revelthat.com>
2020-04-28 20:20:55 +1000

    changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHuRWQkcsNFsCB06db_u5Yv9M7mdwwq8I",
-  authDomain: "expensify-a01b1.firebaseapp.com",
-  databaseURL: "https://expensify-a01b1.firebaseio.com",
-  projectId: "expensify-a01b1",
-  storageBucket: "expensify-a01b1.appspot.com",
-  messagingSenderId: "854625211131",
-  appId: "1:854625211131:web:95839f49121cd4120a565e",
-  measurementId: "G-03NZTXVCG5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
